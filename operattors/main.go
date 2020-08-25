72a71dada7a3689c46a575f0ba4bfadffb0cc040
chobobdev <chobobdev@naver.com>
2020-08-25 14:11:36 +0900

    added subtraction function

--- operattors/main.go
+++ operattors/main.go
@@ -7,10 +7,19 @@ func addition(x, y int) (int, int, int) {
 
 }
 
+func subtraction(x, y int) (int, int, int) {
+	return x, y, x - y
+
+}
+
 func main() {
 	s := "Seongbin" + " " + "Bernie" + " " + "Cho\n" //문자열 Concate 하는 부분은 Python을 연상시킴
 	fmt.Printf("%v", s)                              //문자열의 출력은 C언어를 연상시킴
-	x, y, sum := addition(4, 14)                     //Function에 값을 넣고 받아오기
-	fmt.Printf("%d + %d = %d", x, y, sum)            // Function 에서 return값을 출력
+
+	x, y, sum := addition(4, 14)            //Function에 값을 넣고 받아오기
+	fmt.Printf("%d + %d = %d\n", x, y, sum) // Function 에서 return값을 출력
+
+	x, y, sub := subtraction(14, 4)         //Function에 값을 넣고 받아오기
+	fmt.Printf("%d - %d = %d\n", x, y, sub) // Function 에서 return값을 출력
 
 }
