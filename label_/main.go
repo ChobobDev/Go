74d6c20f4aa5e2bfe3cd6b4c1c7c357945e38410
chobobdev <chobobdev@naver.com>
2020-08-29 23:13:01 +0900

    Label with LOOP in Go lang

--- label_/main.go
+++ label_/main.go
@@ -10,9 +10,23 @@ LOOP:
 				fmt.Println("Start")
 				break LOOP
 			}
-			fmt.Println("Not Yet")
+			fmt.Println("Not Yet") // 실행되지 않는 부분
 		}
-		fmt.Println("Not Yet")
+		fmt.Println("Not Yet") // 실행되지 않는 부분
+	}
+	fmt.Println("Finalized")
+
+LOOP_2:
+	for {
+		for {
+			for {
+				fmt.Println("Start")
+				break
+			}
+			fmt.Println("Not Yet")
+			break LOOP_2
+		}
+		fmt.Println("Not Yet") // 실행되지 않는 부분
 	}
 	fmt.Println("Finalized")
 }
