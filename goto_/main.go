9df9b479c7a47a7c2ebed7df4ce57fef07f67f34
chobobdev <chobobdev@naver.com>
2020-08-29 21:17:05 +0900

    goto function developed

--- goto_/main.go
+++ goto_/main.go
@@ -8,4 +8,9 @@ func main() {
 	fmt.Println("B") // 여기에 있는 모든 것들은 출력되지 않는다.
 L:
 	fmt.Println("This will be Printed")
+	goto M //M 으로 선언된 부분으로 이동한다
+
+M:
+	fmt.Println("This will be Printed as well")
+
 }
