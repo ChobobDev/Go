a8c0b79253d761636c7c1e1208f01fef695b0b60
chobobdev <chobobdev@naver.com>
2020-08-30 21:50:52 +0900

    defer in GO lang

--- defer_/main.go
+++ defer_/main.go
@@ -2,12 +2,14 @@ package main
 
 import "fmt"
 
+// Defer는 함수 안에서 자유롭게 등록될 수 있다
+// 함수 종료시 나중에 등록된 순으로 실행됨
 func runDefer() {
 	defer fmt.Println("DEFER")
 	fmt.Println("DONE")
 }
 
 func main() {
+	fmt.Println("이거 다음")
 	runDefer()
-
 }
