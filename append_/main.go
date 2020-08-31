b2fddd13c05afea0fb999c28b357d1abeef56cfe
chobobdev <chobobdev@naver.com>
2020-08-31 23:08:53 +0900

    append in go

--- append_/main.go
+++ append_/main.go
@@ -6,4 +6,7 @@ func main() {
 	s := []int{1, 2, 3, 4, 5}
 	s = append(s, 6) // s에 append 하는 line
 	fmt.Println(s)
+
+	s = append(s, 7, 8, 9, 10, 11, 12, 13) //s 에 multiple append 하기
+	fmt.Println(s)
 }
