1895fa2007aa2335b36b1c118c821daec0482166
chobobdev <chobobdev@naver.com>
2020-09-28 23:29:53 +0900

    Struct in go

---
+++ struct_/main.go
@@ -0,0 +1,13 @@
+package main
+
+type Person struct {
+	ID         uint
+	name       string
+	department string
+}
+
+p := Person{ID: 20, name: "Seongbin Cho", department: "Dev Relation"}
+
+func main() {
+	
+}
